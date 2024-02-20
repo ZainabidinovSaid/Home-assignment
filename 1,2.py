baec6de2ea16f73db5fc05cc1d4f389534ebceae
unknown <saidislam1233@gmail.com>
2024-02-20 10:57:37 +0600

    first commit

---
+++ 1,2.py
@@ -0,0 +1,13 @@
+#1 question:
+# str, bool, int, float can be used while creating a series object in pandas
+# 2 question
+import pandas as pd
+import numpy as np
+
+mon_number = np.arange(1, 13)
+
+mon_name  = ['January', 'February', 'March', 'April', 'May', "June", 'July', 'August', 'September', 'October', 'November', 'December']
+
+mon_series = pd.Series(data=mon_number, index=mon_name)
+
+print(mon_series)
